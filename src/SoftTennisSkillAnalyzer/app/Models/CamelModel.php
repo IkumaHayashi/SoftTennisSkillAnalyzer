4f39a3cb5e067d047f40bffc386bca163c19a668
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2020-01-31 22:08:27 +0900

    add: modelのプロパティへのアクセスをスネークケースからキャメルケースに変換する

---
+++ src/SoftTennisSkillAnalyzer/app/Models/CamelModel.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model as EloquentModel;
+use Illuminate\Support\Str;
+
+/**
+ * Class CamelModel.
+ *
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+abstract class CamelModel extends EloquentModel
+{
+    public function getAttribute($key)
+    {
+        return parent::getAttribute(Str::snake($key));
+    }
+    
+    public function setAttribute($key, $value)
+    {
+        return parent::setAttribute(Str::snake($key), $value);
+    }    
+}

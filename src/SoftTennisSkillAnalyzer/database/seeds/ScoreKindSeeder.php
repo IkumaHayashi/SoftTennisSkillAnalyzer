d68329f30f694e6a77b135ec55d0ce437def4aa6
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2020-01-31 23:11:23 +0900

    add: score_kindテーブルのシーダー追加

---
+++ src/SoftTennisSkillAnalyzer/database/seeds/ScoreKindSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ScoreKindSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //データの準備
+        $seeds[] = ['name' => 'サービス'
+                    , 'description' => 'プレーを開始するとき(ポイントの最初)の打球'];
+        $seeds[] = ['name' => 'レシーブ'
+                    , 'description' => 'サービスへの返球のこと'];
+        $seeds[] = ['name' => 'ローボレー'
+                    , 'description' => '肩より下の低い球をノーバウンドで返球すること'];
+        $seeds[] = ['name' => 'ハイボレー'
+                    , 'description' => '顔より高い球をノーバウンドで返球すること'];
+        $seeds[] = ['name' => 'ボレー'
+                    , 'description' => '顔あたりの球をノーバウンドで返球すること'];
+        $seeds[] = ['name' => 'スマッシュ'
+                    , 'description' => 'ロビングのボールが落ちてくるところをノーバウンドで打ち込むこと'];
+        $seeds[] = ['name' => 'ストローク'
+                    , 'description' => 'ワンバウンドで返球すること'];
+    
+        //DBへの挿入
+        foreach($seeds as $seed){
+            
+            $scoreKind = new \App\Models\ScoreKind();
+            foreach($seed as $key => $value){
+                $scoreKind->$key = $value;
+            }
+            $scoreKind->save();
+        }
+    }
+}

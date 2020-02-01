1be21420c2f2d469733a0c745921937519c03493
Ikuma Hayashi <ikuma.hayashi.tech@gmail.com>
2020-02-01 15:55:00 +0900

    add: Scoreモデルのファクトリを作成

---
+++ src/SoftTennisSkillAnalyzer/database/factories/ScoreFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+
+$factory->define(\App\Models\Score::class, function (Faker $faker) {
+
+    $players = factory(\App\Models\Player::class, 4)->create();
+    $user = $players[0]->user;
+    foreach ($players as $player) {
+            $player->userId = $user->id;
+            $player->save();
+    }
+    return [
+        'user_id' => $user->id,
+        'player1_1_id' => $players[0]->id,
+        'player1_2_id' => $players[1]->id,
+        'player2_1_id' => $players[2]->id,
+        'player2_2_id' => $players[3]->id,
+    ];
+});
--- src/SoftTennisSkillAnalyzer/tests/Unit/ScoreTest.php
+++ src/SoftTennisSkillAnalyzer/tests/Unit/ScoreTest.php
@@ -16,21 +16,8 @@ class ScoreTest extends TestCase
     public function User、Playerが参照出来る()
     {
         //前準備
-        $players = factory(\App\Models\Player::class, 4)->create();
-        $user = $players[0]->user;
-        foreach ($players as $player) {
-             $player->userId = $user->id;
-             $player->save();
-        }
-
         //実行
-        $score = new \App\Models\Score();
-        $score->userId = $user->id;
-        $score->player1_1Id = $players[0]->id;
-        $score->player1_2Id = $players[1]->id;
-        $score->player2_1Id = $players[2]->id;
-        $score->player2_2Id = $players[3]->id;
-        $score->save();
+        $score = factory(\App\Models\Score::class)->create();
 
         //検証
         $this->assertNotNull($score->user->id);

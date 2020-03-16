1fd7b79f05e606197d21877c18ee2931d0d18cc4
IkumaHayashi <ikuma.hayashi.tech@gmail.com>
2020-03-16 20:31:35 +0900

    update: インデント幅を4に変更

--- src/SoftTennisSkillAnalyzer/app/Http/Controllers/Api/ScoreController.php
+++ src/SoftTennisSkillAnalyzer/app/Http/Controllers/Api/ScoreController.php
@@ -9,107 +9,106 @@ use Illuminate\Support\Facades\Auth;
 
 class ScoreController extends Controller
 {
-  /**
-   * Display a listing of the resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function index()
-  {
-    return Score::with([
-      'player1_a'
-      , 'player1_b'
-      , 'player2_a'
-      , 'player2_b'
-      , 'player1_a.organization'
-      , 'player1_b.organization'
-      , 'player2_a.organization'
-      , 'player2_b.organization'
-      , 'score_type'
-      ])->get();
-  }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Score::where('user_id' ,    Auth::user()->id )->with([
+            'player1_a'
+            , 'player1_b'
+            , 'player2_a'
+            , 'player2_b'
+            , 'player1_a.organization'
+            , 'player1_b.organization'
+            , 'player2_a.organization'
+            , 'player2_b.organization'
+            , 'score_type'
+        ])->get();
+    }
 
-  /**
-   * Show the form for creating a new resource.
-   *
-   * @return \Illuminate\Http\Response
-   */
-  public function create()
-  {
-    // 生成に必要なデータを取得
-    $organization = \App\Models\Organization::get();
-    $score = new \App\Models\Score();
-    return [
-        'oraganization' => $organization,
-        'score_types' => config('score.types'),
-        'game_numbers' => config('score.game_numbers'),
-        'score' => $score,
-    ];
-  }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // 生成に必要なデータを取得
+        $organization = \App\Models\Organization::get();
+        $score = new \App\Models\Score();
+        return [
+                'oraganization' => $organization,
+                'score_types' => config('score.types'),
+                'game_numbers' => config('score.game_numbers'),
+                'score' => $score,
+        ];
+    }
 
-  /**
-   * Store a newly created resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @return \Illuminate\Http\Response
-   */
-  public function store(Request $request)
-  {
-      $score = new Score();
-      \Log::debug($request);
-      $score->player1_a_id = $request->input('player1_a.id');
-      $score->player1_b_id = $request->input('player1_b.id');
-      $score->player2_a_id = $request->input('player2_a.id');
-      $score->player2_b_id = $request->input('player2_b.id');
-      $score->note = $request->input('note');
-      $score->game_numbers = $request->input('game_numbers');
-      $score->score_type_id = $request->input('score_type.id');
-      Auth::user()->scores()->save($score);
-    return response($score, 201);
-  }
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param    \Illuminate\Http\Request    $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+            $score = new Score();
+            $score->player1_a_id = $request->input('player1_a.id');
+            $score->player1_b_id = $request->input('player1_b.id');
+            $score->player2_a_id = $request->input('player2_a.id');
+            $score->player2_b_id = $request->input('player2_b.id');
+            $score->note = $request->input('note');
+            $score->game_numbers = $request->input('game_numbers');
+            $score->score_type_id = $request->input('score_type.id');
+            Auth::user()->scores()->save($score);
+        return response($score, 201);
+    }
 
-  /**
-   * Display the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function show($id)
-  {
-    //
-  }
+    /**
+     * Display the specified resource.
+     *
+     * @param    int    $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
 
-  /**
-   * Show the form for editing the specified resource.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function edit($id)
-  {
-    //
-  }
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param    int    $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
-  /**
-   * Update the specified resource in storage.
-   *
-   * @param  \Illuminate\Http\Request  $request
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function update(Request $request, $id)
-  {
-    //
-  }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param    \Illuminate\Http\Request    $request
+     * @param    int    $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
 
-  /**
-   * Remove the specified resource from storage.
-   *
-   * @param  int  $id
-   * @return \Illuminate\Http\Response
-   */
-  public function destroy($id)
-  {
-    //
-  }
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param    int    $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
